d5089b9fd2c16932f7923fa9f247b418e63726f7
sahil <sahil9955555@gmail.com>
2026-04-06 17:00:05 +0530

    Updated test

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,8 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
+
+  
   {
     files: ['**/*.{js,jsx}'],
     extends: [
@@ -15,7 +17,10 @@ export default defineConfig([
     ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
@@ -24,6 +29,14 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    },
+  },
+
+  
+  {
+    files: ['**/*.test.{js,jsx}'],
+    languageOptions: {
+      globals: globals.vitest,
     },
   },
 ])
