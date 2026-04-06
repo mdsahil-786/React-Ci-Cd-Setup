0816db98fb8a17e3f8e3f7556a535855a08c2a19
sahil <sahil9955555@gmail.com>
2026-04-06 16:27:40 +0530

    updated ci.yml and added dist

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,6 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.js', // Add this line if you have a setup file
+    exclude:[...configDefaults.exclude,'e2e/*'],
   },
 })
